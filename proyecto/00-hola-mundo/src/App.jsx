f21fc3939d9badd0bcbbc91879c8ba9fbc87780b
Matías Bravo <mati.bravo7@gmail.com>
2025-02-04 11:59:00 -0300

    se finaliza componente Twitter card

--- proyecto/00-hola-mundo/src/App.jsx
+++ proyecto/00-hola-mundo/src/App.jsx
@@ -2,14 +2,41 @@ import { TwitterFollowCard } from "./TwitterFollowCard"
 import "./index.css"
 
 export function App () {
+
+    const usuarios = [
+            {
+                id: 1,
+                userName: 'kikobeats',
+                name: 'kiko Beats',
+                isFollowing: false
+            },
+            {
+                id: 2,
+                userName: 'midudev',
+                name: 'Miguel Duran',
+                isFollowing: false
+            },
+            {
+                id: 3,
+                userName: 'unknown',
+                name: 'Nadie lo conoce',
+                isFollowing: false
+            }
+        ]
+
     return (
         <section className="App">
-            <TwitterFollowCard userName= 'kikobeats' name= 'Kiko Beats' />
-            <TwitterFollowCard userName= 'midudev' name= 'Miguel Duran' />
-            <TwitterFollowCard userName= 'kikobeats' name= 'Kiko Beats' />
-            <TwitterFollowCard userName= 'midudev' name= 'Miguel Duran' />
-            <TwitterFollowCard />
+            {
+                //destructurando los usuarios, y retornando el elemento con las propiedades de los usuarios destructurados
+                usuarios.map(({id, userName, name, isFollowing}) => (
+                    
+                    <TwitterFollowCard
+                            key= {id} 
+                            userName= {userName}
+                            name= {name}
+                            isFollowing= {isFollowing} />
+                ))
+            }
         </section>
-        
     )    
 }
